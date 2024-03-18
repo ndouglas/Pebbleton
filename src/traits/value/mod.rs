e59898bfbaf05176599ef51b648353126ed186e6
Nathan Douglas <github@darkdell.net>
2024-03-18 08:11:17 -0400

    Remove some stuff.

--- src/traits/value/mod.rs
+++ src/traits/value/mod.rs
@@ -2,7 +2,6 @@
 pub mod macros;
 
 pub mod bool;
-pub mod entity;
 pub mod r#enum;
 pub mod float;
 pub mod float_range;
@@ -12,7 +11,6 @@ pub mod string;
 
 pub mod prelude {
   pub use super::bool::*;
-  pub use super::entity::*;
   pub use super::float::*;
   pub use super::float_range::*;
   pub use super::int::*;
